2ab8a640161ddff33d00815954786f9d4a03195d
gregory <grish2ry@gmail.com>
2026-09-12 13:22:16 +0300

    query added

--- src/eval/expressions.rs
+++ src/eval/expressions.rs
@@ -1,18 +1,33 @@
 use std::{fmt::Debug, sync::Arc};
 
+use anyhow::Result;
 use async_trait::async_trait;
-use sqlx::PgPool;
+use sqlx::{PgPool, query_as};
+
+use super::EvaluatedExpression;
 
 #[async_trait]
 pub trait Expressions: Debug + Send + Sync + 'static {
-    // TODO
+    async fn history(&self, account_id: i32) -> Result<Vec<EvaluatedExpression>>;
 }
 
 pub type DynExpressions = Arc<dyn Expressions>;
 
 #[async_trait]
 impl Expressions for PgPool {
-    // TODO
+    async fn history(&self, account_id: i32) -> Result<Vec<EvaluatedExpression>> {
+        Ok(query_as::<_, EvaluatedExpression>(
+            r#"
+            SELECT expression, result
+            FROM evaluated_expression
+            WHERE account = $1
+            ORDER BY id DESC
+            "#,
+        )
+        .bind(account_id)
+        .fetch_all(self)
+        .await?)
+    }
 }
 
 #[cfg(test)]
